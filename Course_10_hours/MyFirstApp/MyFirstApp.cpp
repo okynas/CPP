bf33fec19821caf4e780ddf7cdab5df33c48cb61
Stian Martinsen <36879451+okynas@users.noreply.github.com>
2022-03-28 17:35:34 +0200

    FEAT: prograss 50 min in video

--- Course_10_hours/MyFirstApp/MyFirstApp.cpp
+++ Course_10_hours/MyFirstApp/MyFirstApp.cpp
@@ -1,10 +1,12 @@
 // MyFirstApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    cout << "Hello World!\n";
+    system("pause>0");
 }
 
---
+++ Course_10_hours/Tutorial2/Tutorial2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>;
+
+
+using namespace std;
+
+int main()
+{
+    float annualSalary;
+    cout << "Please enter your annual sallary: ";
+    cin >> annualSalary;
+    float monthlySalary = annualSalary / 12;
+    cout << "Your montyly salary is " << monthlySalary << endl;
+    cout << "In 10 years , you will have" << annualSalary * 10;
+
+    // naming variables:
+
+    /*
+    
+    valid:
+
+    char s = 'a';
+    char minString = 'a';
+    char MinString = 'a';
+    char MIN_STRING = 'a';
+    char LANG_STRING_ = 'a';
+    char tallIVariabelNavn56 = 'a';
+    char tall5MidtI = 'a';
+
+    invalid:
+    char 5 = 'a';
+    char 6stringNavn = 'a';
+    char !star = 'a';
+    char star! = 'a';
+    char mellom rom = 'a';
+
+    */
+    char character = 'a';
+    
+
+    system("pause>0");
+    
+}
+
