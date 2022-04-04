440420a9c9a58c41b62a98db353296b652032234
Stian Martinsen <36879451+okynas@users.noreply.github.com>
2022-04-04 21:01:40 +0200

    FEAT: references and pointers

--- The_Cherno_Tutorials/LearnPointers/LearnPointers.cpp
+++ The_Cherno_Tutorials/LearnPointers/LearnPointers.cpp
@@ -7,13 +7,40 @@ EACH address is 1 BYTE of data = 8 BIT
 #include <iostream>
 using namespace std;
 
-#define LOG(X) cout << x << endl;
+#define LOG(x) cout << x << endl;
+
+void Increment(int& value) {
+    value++;
+}
 
 int main()
 {
-    int var = 8;
+    /*int var = 8;
 
     // void pointer = typeless pointer.
-    void* ptr = &var;
-    cin.get();
+    int* ptr = &var;
+    *ptr = 10;
+    LOG(var);*/
+
+    // allocate 8 bytes of memory to a buffer char-array.
+    
+    /* char* buffer = new char[8];
+    memset(buffer, 0, 8); */
+    
+    // pnt to a pnt
+    /* char** ptr = &buffer; */
+
+    // delete / remove the allocated 8 bytes for memory
+    /* delete[] buffer; */
+
+    int a = 5;
+    int b = 8;
+
+    int& ref = a;
+    ref = b;
+    
+    Increment(a);
+    LOG(a);
+
+    cin.get(); 
 }
